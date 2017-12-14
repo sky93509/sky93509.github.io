928f12d5ac658a80b0374c2c434cd745b7326137
sky93509 <33918907+sky93509@users.noreply.github.com>
2017-12-14 12:42:11 +0800

    Add files via upload

--- commandblocks.js
+++ commandblocks.js
@@ -3,6 +3,18 @@ ext._shutdown = function(){};
 ext._getStatus = function(){
 return{status: 2, msg: 'Ready'};
 };
+ext.wait_random = function(callback){
+	wait = Math.random();
+	console.log('Waiting for' + wait + ' second');
+	window.setTimeout(function(){
+		callback();
+	},wait*1000);
+};
+var descriptor = {
+	blocks: [
+	['w', 'wait for random time', 'wait_random'],
+	]
+};
 ext.my_first_block = function(){
 };
 var descriptor = {
@@ -10,5 +22,5 @@ blocks:[
 [' ', 'my first block', 'my_first_block'],
 ]
 };
-ScratchExtensions.register('My first extension', descriptor, ext);
+ScratchExtensions.register('My first extension','Random wait extension', descriptor, ext);
 })({});
