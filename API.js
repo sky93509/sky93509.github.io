fb870131b5fa22a705e8c4d94c3d516da5c7b10e
sky93509 <33918907+sky93509@users.noreply.github.com>
2017-12-16 10:19:31 +0800

    Add files via upload

--- API.js
+++ API.js
@@ -11,7 +11,7 @@
     ext.get_temp = function(location, callback) {
         // Make an AJAX call to the Open Weather Maps API
         $.ajax({
-              url: 'http://api.openweathermap.org/data/2.5/weather?q=Hurzuf,UA',
+              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
               dataType: 'jsonp',
               success: function( weather_data ) {
                   // Got the data - parse it and return the temperature
