e0b83d08dfe5e10a32bbdf2ea79634f52256e93f
sky93509 <33918907+sky93509@users.noreply.github.com>
2017-12-18 16:48:29 +0800

    Update API.js

--- API.js
+++ API.js
@@ -1,33 +1,99 @@
-(function(ext) {
-    // Cleanup function when the extension is unloaded
+new (function() {
+	window.include = function(srclist, cb){
+		if(!cb) cb = function(){};
+		var requireModule = (function(index){
+			var moduleName = Object.keys(srclist)[index];
+			var src = srclist[moduleName];
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', src, true);
+			xhr.onreadystatechange = function(){
+				if(xhr.readyState !== 4) return;
+
+				if(xhr.status === 200){
+					//모듈 변수 설정
+					var module = {
+						_exports: {}
+					};
+
+					Object.defineProperty(module, 'exports', {
+						get: (function(){
+							return this._exports;
+						}).bind(module),
+
+						set: (function(v){
+							this._exports = v;
+						}).bind(module)
+					});
+
+					//XHR로 받아온 결과물 eval
+					try{
+						(new Function('module', 'exports', xhr.responseText))(module, module.exports);
+					}catch(err){
+						cb(err);
+						return;
+					}
+
+					//exports를 global에 모듈명으로 저장
+					window[moduleName] = module.exports;
+
+					//마지막까지 갈 경우 끝냄
+					if(Object.keys(srclist).length <= index + 1){
+						cb(null);
+					}else{
+						setTimeout(function(){
+							requireModule(index + 1);
+						}, 0);
+					}
+				}else{
+					cb(new Error('Error while requesting module from server (Status ' + xhr.status + ') : ' + moduleName));
+				}
+			};
+
+			xhr.send(null);
+			console.log('Loading a module : ' + moduleName);
+		});
+
+		requireModule(0);
+	};
+
+	include([
+		'https://raw.githubusercontent.com/taylorhakes/promise-polyfill/master/promise.min.js',
+		'https://raw.githubusercontent.com/github/fetch/master/fetch.js'
+	]);
+
+    var ext = this;
     ext._shutdown = function() {};
 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.get_temp = function(location, callback) {
-        // Make an AJAX call to the Open Weather Maps API
-        $.ajax({
-              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
-              dataType: 'jsonp',
-              success: function( weather_data ) {
-                  // Got the data - parse it and return the temperature
-                  temperature = weather_data['main']['temp'];
-                  callback(temperature);
-              }
-        });
+    ext.get_ajax = function(url, callback){
+    	fetch(url).then(function(v){
+    		return v.text();
+    	}).then(function(v){
+    		callback(v);
+    	});
     };
 
-    // Block and block menu descriptions
+    ext.post_ajax = function(url, data, callback){
+    	fetch(url, {
+    		method: 'POST',
+    		body: data
+    	}).then(function(v){
+    		return v.text();
+    	}).then(function(v){
+    		callback(v);
+    	});
+    };
+
     var descriptor = {
         blocks: [
-            ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA'],
+            ['R', 'GET %s', 'get_ajax', 'http://example.com'],
+            ['R', 'POST %s with content %s', 'post_ajax', 'https://example.com', '{name: "Khinenw"}']
         ]
     };
 
     // Register the extension
-    ScratchExtensions.register('Weather extension', descriptor, ext);
-})({});
+    ScratchExtensions.register('XMLHttpRequest Extension', descriptor, ext);
+})();
